6dab3a50d2bc084369c3d32550d1fee5acd0dd8e
sudhakarreddy shapathi <sudhakar@pingalasoftware.com>
2022-06-06 14:44:47 +0530

    modified

--- functions.py
+++ functions.py
@@ -40,4 +40,37 @@ my_function(child1="abc",child2="cdf",child3 ="ssr")
 
 
 
-#
+#if we dont know howmany key words are passed to function then add ** to the parameter#
+def my_function(**kid):
+  print("his last a name" + kid["lname"])
+my_function(fname="abc",lname=" shapthi")
+
+
+#using default  parameter Value#without argument we can use default value##
+def my_function(country=" india"):
+    print("Iam from" + country)
+my_function()#here mentioned key value #
+my_function("usa")#here directly called #
+my_function("uk")
+
+
+#we can send any data type arugument inside a function it is consider as same data type inside function##
+def my_function(food):
+    for x in food:
+        print(x)
+fruits =["apple","bannaa","cherry"]
+my_function(fruits)
+
+
+#using return statement return a value##
+def my_function(x):
+    return 5 * x
+print(my_function(3))
+print(my_function(6))
+print(my_function(8))
+
+#using pass statement we will avoid error#
+#function definition cant be empty ,if it is empty then will get error ,by using  pass statement avoid getting error##
+
+def myfunction():
+    pass
