4c048742d63818a44c99e6babe8c52f74511dc08
sudhakarreddy shapathi <sudhakar@pingalasoftware.com>
2022-06-13 13:27:31 +0530

    commit

---
+++ maths.py
@@ -0,0 +1,54 @@
+##python has built in math functions## including extensive module that allows you to perform mathematical tasks on numbers##
+
+
+#using min() max()  we can find the lowest&highest value in an iterable#
+
+x=min(5,10,15)
+x=max(5,10,15)
+print(x)
+print(x)
+
+
+#the abs() function returns the positive value of the specified number##
+
+x= abs(-6.33)
+print(x)
+
+
+#the pow(x,y) function returns the value of x to the power of y(xpower y)##
+x= pow(4,6)
+print(x)
+
+
+#math module#python has also aa build in module called math#its iextends the mathematical functions ,
+#to use it,we must import the math module#
+
+#the "math.sqrt()" method returns the square root of a number##
+
+import math
+x=math.sqrt(81)
+print(x)#op-9":
+
+
+
+#The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer,and returns the result##
+
+import math
+x= math.ceil(2.5)
+y= math.floor(1.4)
+print(x)#returs 3
+print(y)#its returns 1
+
+
+
+
+
+
+#the math.pi constant value of pi=3.141...#
+
+import math
+x= math.pi
+print(x)#return the "pi" value#
+
+
+
